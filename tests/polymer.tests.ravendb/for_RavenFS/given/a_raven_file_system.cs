def27257a67d5691333886a87a1d3209702f2344
RyanMBritton <ryanbritton.archangel@gmail.com>
2014-12-07 12:30:08 +0200

    added tests for basic RavenFS interaction

---
+++ tests/polymer.tests.ravendb/for_RavenFS/given/a_raven_file_system.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+using Raven.Client.FileSystem;
+using System;
+using System.IO;
+
+namespace polymer.tests.ravendb.for_RavenFS.given
+{
+	public class a_raven_file_system
+	{
+		Establish context = async () =>
+		{
+			_fileStore = new FilesStore() { Url = "http://localhost:8088", DefaultFileSystem = "TST" };
+			_fileStore.Initialize();
+			_currentDir = AppDomain.CurrentDomain.BaseDirectory;
+			_testFile = Path.Combine(_currentDir, "_assets", "test.txt");
+		};
+
+		protected static IFilesStore _fileStore;
+		protected static string _currentDir;
+		protected static string _testFile;
+	}
+}
---
+++ tests/polymer.tests.ravendb/for_RavenFS/when_adding_a_new_file.cs
@@ -0,0 +1,37 @@
+﻿
+using Machine.Specifications;
+using System.IO;
+
+namespace polymer.tests.ravendb.for_RavenFS
+{
+	public class when_adding_a_new_file : given.a_raven_file_system
+	{
+		Establish context = () =>
+		{
+
+		};
+
+		Because because = async () =>
+		{
+			_stream = new FileStream(_testFile, FileMode.Open);
+			using (var session = _fileStore.OpenAsyncSession())
+			{
+				session.RegisterUpload(_testFile, _stream);
+				await session.SaveChangesAsync();
+			}
+		};
+
+		It should_pass = () => true.ShouldBeTrue();
+		It should_contain_file_in_database = async () =>
+		{
+			using (var session = _fileStore.OpenAsyncSession())
+			{
+				var fileTask = session.Query().WhereEquals(x => x.Name, "test.txt").FirstOrDefaultAsync();
+				fileTask.Wait();
+				var file = fileTask.Result;
+				file.ShouldNotBeNull();
+			}
+		};
+		private static FileStream _stream;
+	}
+}
